1f96eb451d6a5a50e0ad59606d1a381938b14fc5
hamzalhadouchi <hamzabniss16@gmail.com>
2025-10-06 19:35:08 +0100

    feat : Add getter and setter methods to UtilisateurDTO for enhanced data manipulation https://hamzaalhadouchi-1735.atlassian.net/browse/TEM-9

--- src/main/java/java/tele_expertise/dto/UtilisateurDTO.java
+++ src/main/java/java/tele_expertise/dto/UtilisateurDTO.java
@@ -29,4 +29,68 @@ public class UtilisateurDTO {
         this.dateCreation = dateCreation;
         this.actif = actif;
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public RoleUtilisateur getRole() {
+        return role;
+    }
+
+    public void setRole(RoleUtilisateur role) {
+        this.role = role;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public LocalDateTime getDateCreation() {
+        return dateCreation;
+    }
+
+    public void setDateCreation(LocalDateTime dateCreation) {
+        this.dateCreation = dateCreation;
+    }
+
+    public Boolean getActif() {
+        return actif;
+    }
+
+    public void setActif(Boolean actif) {
+        this.actif = actif;
+    }
 }
