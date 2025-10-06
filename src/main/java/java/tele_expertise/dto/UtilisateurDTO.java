163213088d06f57547ce10e67c2de6ed7a190ea0
hamzalhadouchi <hamzabniss16@gmail.com>
2025-10-06 19:33:53 +0100

    feat : Enhance UtilisateurDTO with additional fields for user details
    https://hamzaalhadouchi-1735.atlassian.net/browse/TEM-9

--- src/main/java/java/tele_expertise/dto/UtilisateurDTO.java
+++ src/main/java/java/tele_expertise/dto/UtilisateurDTO.java
@@ -1,4 +1,17 @@
 package java.tele_expertise.dto;
 
+import enums.RoleUtilisateur;
+
+import java.time.LocalDateTime;
+
 public class UtilisateurDTO {
+
+        private Long id;
+        private String nom;
+        private String prenom;
+        private String email;
+        private RoleUtilisateur role;
+        private String telephone;
+        private LocalDateTime dateCreation;
+        private Boolean actif;
 }
