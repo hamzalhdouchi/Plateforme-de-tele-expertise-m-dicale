06c8ed04de7af26159ce525ed6cd88f411ed2e93
hamzalhadouchi <hamzabniss16@gmail.com>
2025-10-06 19:34:29 +0100

    feat : Add constructors to UtilisateurDTO for improved object initialization
    https://hamzaalhadouchi-1735.atlassian.net/browse/TEM-9

--- src/main/java/java/tele_expertise/dto/UtilisateurDTO.java
+++ src/main/java/java/tele_expertise/dto/UtilisateurDTO.java
@@ -14,4 +14,19 @@ public class UtilisateurDTO {
         private String telephone;
         private LocalDateTime dateCreation;
         private Boolean actif;
+
+    public UtilisateurDTO() {}
+
+    public UtilisateurDTO(Long id, String nom, String prenom, String email,
+                          RoleUtilisateur role, String telephone,
+                          LocalDateTime dateCreation, Boolean actif) {
+        this.id = id;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.email = email;
+        this.role = role;
+        this.telephone = telephone;
+        this.dateCreation = dateCreation;
+        this.actif = actif;
+    }
 }
