67af98af50a2ed27f97def9fd62a8796fcef7d96
hamzalhadouchi <hamzabniss16@gmail.com>
2025-10-08 15:24:29 +0100

    feat : Implement SignesVitauxMapper methods for entity-DTO conversion https://hamzaalhadouchi-1735.atlassian.net/browse/TEM-9

--- src/main/java/tele_expertise/mapper/mappers/SignesVitauxMapper.java
+++ src/main/java/tele_expertise/mapper/mappers/SignesVitauxMapper.java
@@ -1,4 +1,38 @@
-package tele_expertise.mapper.mappers;
+package tele_expertise.mapper;
+
+import tele_expertise.entity.SignesVitaux;
+import tele_expertise.dto.SignesVitauxDTO;
 
 public class SignesVitauxMapper {
+
+    public static SignesVitauxDTO toDTO(SignesVitaux entity) {
+        if (entity == null) {
+            return null;
+        }
+
+        return new SignesVitauxDTO(
+                entity.getId(),
+                entity.getTemperature(),
+                entity.getTensionsystolique(),
+                entity.getFrequencerespiratoire(),
+                entity.getTensiondiastolique(),
+                entity.getSaturation()
+        );
+    }
+
+    public static SignesVitaux toEntity(SignesVitauxDTO dto) {
+        if (dto == null) {
+            return null;
+        }
+
+        SignesVitaux entity = new SignesVitaux();
+        entity.setId(dto.getId());
+        entity.setTemperature(dto.getTemperature());
+        entity.setTensionsystolique(dto.getTensionSystolique());
+        entity.setFrequencerespiratoire(dto.getFrequenceRespiratoire());
+        entity.setTensiondiastolique(dto.getTensionDiastolique());
+        entity.setSaturation(dto.getSaturation());
+
+        return entity;
+    }
 }
