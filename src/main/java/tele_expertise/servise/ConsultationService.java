6f6a9237326b6c2b81429c590c85eaee60dc88ca
hamzalhadouchi <hamzabniss16@gmail.com>
2025-10-10 16:34:09 +0100

    feat : Implement ConsultationDAO integration in ConsultationService for saving consultations https://hamzaalhadouchi-1735.atlassian.net/browse/TEM-9

--- src/main/java/tele_expertise/servise/ConsultationService.java
+++ src/main/java/tele_expertise/servise/ConsultationService.java
@@ -1,4 +1,22 @@
 package tele_expertise.servise;
 
+
+import tele_expertise.dao.ConsultationDAO;
+import tele_expertise.entity.Consultation;
+
 public class ConsultationService {
-}
+    private ConsultationDAO consultationDAO;
+
+    public ConsultationService() {
+        this.consultationDAO = new ConsultationDAO();
+    }
+
+    public Consultation save(Consultation consultation) {
+        return consultationDAO.save(consultation);
+    }
+
+    public void close() {
+        if (consultationDAO != null) {
+            consultationDAO.close();
+        }
+    }
