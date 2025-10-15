5b636fee59fbd21ee5c10f2f47b000b933c901f2
hamzalhadouchi <hamzabniss16@gmail.com>
2025-10-15 09:50:16 +0100

    feat : Add methods for finding and updating Consultation entities in ConsultationDAO https://hamzaalhadouchi-1735.atlassian.net/browse/TEM-41

--- src/main/java/tele_expertise/dao/ConsultationDAO.java
+++ src/main/java/tele_expertise/dao/ConsultationDAO.java
@@ -3,13 +3,37 @@ package tele_expertise.dao;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
+import jakarta.persistence.TypedQuery;
 import tele_expertise.entity.Consultation;
+
+import java.util.List;
 
 public class ConsultationDAO {
     private EntityManagerFactory emf;
 
     public ConsultationDAO(EntityManagerFactory emf) {
         this.emf = emf;
+    }
+
+    public Consultation findById(Long id) {
+        EntityManager em = emf.createEntityManager();
+        try {
+            return em.find(Consultation.class, id);
+        } finally {
+            em.close();
+        }
+    }
+
+    public List<Consultation> findByPatientId(int patientId) {
+        EntityManager em = emf.createEntityManager();
+        try {
+            return em.createQuery(
+                            "SELECT c FROM Consultation c WHERE c.patient.id = :patientId", Consultation.class)
+                    .setParameter("patientId", patientId)
+                    .getResultList();
+        } finally {
+            em.close();
+        }
     }
 
     public Consultation save(Consultation consultation) {
@@ -40,4 +64,51 @@ public class ConsultationDAO {
 //            emf.close();
 //        }
 //    }
+
+    public List<Consultation> getConsultationsRecentes(Long medecinId) {
+        EntityManager em = emf.createEntityManager();
+        try {
+            String jpql = "SELECT c FROM Consultation c " +
+                    "JOIN FETCH c.patient p " +
+                    "WHERE c.medecinGeneraliste.id = :idMedecin " +
+                    "ORDER BY c.dateConsultation DESC";
+
+            TypedQuery<Consultation> query = em.createQuery(jpql, Consultation.class);
+            query.setParameter("idMedecin", medecinId);
+            return query.getResultList();
+        } finally {
+            em.close();
+        }
+    }
+
+    public void update(Consultation consultation) {
+        EntityManager em = emf.createEntityManager();
+        try {
+            em.getTransaction().begin();
+            em.merge(consultation);
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            if (em.getTransaction().isActive()) {
+                em.getTransaction().rollback();
+            }
+            throw new RuntimeException("Erreur lors de la mise à jour de la consultation", e);
+        } finally {
+            em.close();
+        }
+    }
+
+    public List<Consultation> getAll() {
+        EntityManager em = emf.createEntityManager();
+        try {
+            List<Consultation> list = em.createQuery(
+                            "SELECT c FROM Consultation c LEFT JOIN FETCH c.patient", Consultation.class)
+                    .getResultList();
+            return list;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            em.close();
+        }
+    }
 }
