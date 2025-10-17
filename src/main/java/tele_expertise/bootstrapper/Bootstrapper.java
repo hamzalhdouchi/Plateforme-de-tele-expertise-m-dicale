cb2283e0dc28b0d607e32ee375789113e9923cea
hamzalhadouchi <hamzabniss16@gmail.com>
2025-10-17 18:28:22 +0100

    FIX : Refactor Bootstrapper to streamline service initialization and improve dependency management https://hamzaalhadouchi-1735.atlassian.net/browse/TEM-67

--- src/main/java/tele_expertise/bootstrapper/Bootstrapper.java
+++ src/main/java/tele_expertise/bootstrapper/Bootstrapper.java
@@ -1,10 +1,7 @@
 package tele_expertise.bootstrapper;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
-import tele_expertise.dao.ConsultationDAO;
-import tele_expertise.dao.PatientImpl;
-import tele_expertise.dao.SignesVitauxImpl;
-import tele_expertise.dao.UtilisateurDaoImpl;
+import tele_expertise.dao.*;
 import tele_expertise.dto.PatientDTO;
 import tele_expertise.dto.SignesVitauxDTO;
 import tele_expertise.dto.UtilisateurDTO;
@@ -24,8 +21,9 @@ import jakarta.servlet.annotation.WebListener;
 
             EntityManagerFactory emf = Persistence.createEntityManagerFactory("tele_expertisePU");
             UtilisateurDaoImpl UtlistaeurImpl = new UtilisateurDaoImpl(emf);
-            SpecialiteServiceImpl svi = new SpecialiteServiceImpl(emf);
-            UserServiceImpl userService = new UserServiceImpl(UtlistaeurImpl,svi);
+            SpecialiteDaoImpl specialiteDao = new SpecialiteDaoImpl(emf);
+            SpecialiteServiceImpl specialiteService = new SpecialiteServiceImpl(specialiteDao);
+            UserServiceImpl userService = new UserServiceImpl(UtlistaeurImpl,specialiteService);
             UtilisateurDTO  utilisateurDTO = new UtilisateurDTO();
             SignesVitauxImpl  sv = new SignesVitauxImpl(emf);
             SignesVitauxService servicsSinng = new  SignesVitauxService(sv);
@@ -33,7 +31,20 @@ import jakarta.servlet.annotation.WebListener;
             ConsultationDAO consultationDAO = new ConsultationDAO(emf);
             ConsultationService consultationService = new ConsultationService(consultationDAO);
             PatientService service = new PatientService(patient);
+            CreneauService creneauService = new CreneauService(emf);
+            SpecialisteService specialisteService = new SpecialisteService(emf);
+            DemandeExpertiseDAO demandeExpertiseDAO = new DemandeExpertiseDAO(emf);
+            DemandeExpertiseService demandeExpertiseService = new DemandeExpertiseService(demandeExpertiseDAO);
 
+
+
+
+            sce.getServletContext().setAttribute("specialisteService", specialisteService);
+            sce.getServletContext().setAttribute("creneauService", creneauService);
+
+
+            sce.getServletContext().setAttribute("demandeExpertiseService", demandeExpertiseService);
+            sce.getServletContext().setAttribute("specialiteService", specialiteService);
 
             sce.getServletContext().setAttribute("Sign", sv);
             sce.getServletContext().setAttribute("patientService", service);
